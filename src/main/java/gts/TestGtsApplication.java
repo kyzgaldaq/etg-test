6ff91982609ae70d3fb4111e68f7129244c0c0f4
Ainazik <a.toktomamatova@rsktech.kg>
2025-05-23 11:11:19 +0600

    initial com

---
+++ src/main/java/gts/TestGtsApplication.java
@@ -0,0 +1,13 @@
+package gts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGtsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGtsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gts/TestGtsApplicationTests.java
@@ -0,0 +1,13 @@
+package gts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGtsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
